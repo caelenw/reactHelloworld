66b7e126884163898d7cb2320ee9bee5daad99e7
caelen wall <cswall@email.sc.edu>
2024-10-22 13:12:18 -0400

    update

---
+++ my-react-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <p>Hello Welcome to hello world for react</p>
+  </React.StrictMode>
+);
+
